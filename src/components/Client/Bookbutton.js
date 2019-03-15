3bd679c1339c8a29709c3c7c71142de8481f34da
DaltonHart <DaltonVaren@gmail.com>
2019-03-15 11:07:15 -0700

    added parralax and started work on modal

---
+++ src/components/Client/Bookbutton.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Button, Header, Image, Modal, Icon, Grid } from 'semantic-ui-react'
+
+class Bookbutton extends Component {
+  state = { modalOpen: false }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+  render() {
+    return (
+      <div className="bookbutton">
+        <Modal 
+        trigger={<Button onClick={this.handleOpen} id='book' color='yellow'>Book an Appointment</Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        >
+          <Modal.Header>
+            <Button color='red' onClick={this.handleClose}>
+                X
+            </Button>
+            <h1>
+              A few Questions to get you ready.
+            </h1>
+          </Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                Options to accept will be here.
+              </Modal.Description>
+            </Modal.Content>
+            
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Bookbutton;
--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import Parallax from 'react-springy-parallax'
+import Bookbutton from './Bookbutton'
 import wood from '../assets/wood.jpg'
 class Client extends Component {
   render() {
     let style = {
-      backgroundImage: `url(${wood})`
+      // backgroundImage: `url(${wood})`
+      backgroundColor: `red`
     }
     return (
-      <div style={style} className="clientpg">
+      <div className="clientpg">
         <div className="over">
-
+        <Bookbutton/>
+        <Parallax  ref='parallax' pages={2}>
+          <Parallax.Layer
+              offset={0}
+              speed={0.5}>
+              <span >Layers can contain anything</span>
+          </Parallax.Layer>
+          <Parallax.Layer
+              style={style}
+              offset={1}
+              speed={0.5}>
+              <span>Layers can contain almost anything</span>
+          </Parallax.Layer>
+          </Parallax>
         </div>
       </div>
     );
